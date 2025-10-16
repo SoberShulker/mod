8a2dd67eb6d839e97646c040cf39e429c2982417
SoberShulker <91487903+SoberShulker@users.noreply.github.com>
2025-10-16 22:49:26 +0200

    update tickhandler
    it somewhat works (ill fix it later (no i wont))

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,19 +1,41 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-@Mod(modid = com.example.examplemod.ExampleMod.MODID, version = com.example.examplemod.ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod {
+    public static final String MODID = "jumpmod";
+    public static final String NAME = "Auto Jump Mod";
     public static final String VERSION = "1.0";
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    private int tickCounter = 0;
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) {
+            tickCounter++;
+
+            if (tickCounter >= 10) { // every 10 ticks
+                EntityPlayer player = event.player;
+
+                if (player.onGround) {
+                    player.jump();
+                    player.addChatMessage(new ChatComponentText("You jumped!"));
+                }
+
+                tickCounter = 0;
+            }
+        }
     }
 }
